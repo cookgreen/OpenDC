e6e8ab24783b7b7e1875a71ce718a10bbfc3622f
Cook Green <dell20091234@gmail.com>
2019-09-21 13:20:25 +0800

    Add SPR and BTS loaders

---
+++ OpenRA.Mods.DarkColony/SpriteLoaders/BTSLoader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenRA.Graphics;
+using OpenRA.Primitives;
+
+namespace OpenRA.Mods.DarkColony.SpriteLoaders
+{
+	public class BTSLoader : ISpriteLoader
+	{
+		public int TilesNum { get; set; }
+		class BTSFrame : ISpriteFrame
+		{
+			public Size Size { get; set; }
+
+			public Size FrameSize { get; set; }
+
+			public float2 Offset { get; set; }
+
+			public byte[] Data { get; set; }
+
+			public bool DisableExportPadding { get; set; }
+		}
+
+		ISpriteFrame[] ParseFrames(BinaryReader reader)
+		{
+			List<BTSFrame> frames = new List<BTSFrame>();
+			for (int i = 0; i < TilesNum; i++)
+			{
+				int id = reader.ReadInt32();
+				BTSFrame frame = new BTSFrame();
+				frame.Size = new Size(32, 32);
+				frame.Data = new byte[32 * 32];
+				for (int j = 0; j < frame.Data.Length; j++)
+				{
+					frame.Data[j] = reader.ReadByte();
+				}
+
+				frames.Add(frame);
+			}
+
+			return frames.ToArray();
+		}
+
+		public bool TryParseSprite(Stream s, out ISpriteFrame[] frames, out TypeDictionary metadata)
+		{
+			metadata = null;
+			using (BinaryReader reader = new BinaryReader(s))
+			{
+				int unknown = reader.ReadInt32();
+				TilesNum = reader.ReadInt32();
+
+				byte[] palette = new byte[256 * 3];
+				for (int i = 0; i < palette.Length; i++)
+				{
+					palette[i] = reader.ReadByte();
+				}
+
+				frames = ParseFrames(reader);
+			}
+
+			return true;
+		}
+	}
+}
---
+++ OpenRA.Mods.DarkColony/SpriteLoaders/SPRLoader.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenRA.Graphics;
+using OpenRA.Primitives;
+
+namespace OpenRA.Mods.DarkColony.SpriteLoaders
+{
+	public class SPRLoader : ISpriteLoader
+	{
+		public ushort FrameNum { get; set; }
+
+		class SPRFrame : ISpriteFrame
+		{
+			public Size Size { get; set; }
+
+			public Size FrameSize { get; set; }
+
+			public float2 Offset { get; set; }
+
+			public byte[] Data { get; set; }
+
+			public bool DisableExportPadding { get; set; }
+		}
+
+		ISpriteFrame[] ParseFrames(BinaryReader reader)
+		{
+			List<SPRFrame> frames = new List<SPRFrame>();
+
+			for (int i = 0; i < FrameNum; i++)
+			{
+				SPRFrame frame = new SPRFrame();
+				ushort width = reader.ReadUInt16();
+				ushort height = reader.ReadUInt16();
+				var unknown1 = reader.ReadUInt16();
+				var unknown2 = reader.ReadUInt16();
+				frame.Size = new Size(width, height);
+				frame.Data = new byte[width * height];
+				frames.Add(frame);
+			}
+
+			for (int i = 0; i < FrameNum; i++)
+			{
+				for (int j = 0; j < frames[i].Data.Length; j++)
+				{
+					frames[i].Data[j] = reader.ReadByte();
+				}
+			}
+
+			return frames.ToArray();
+		}
+
+		public bool TryParseSprite(Stream s, out ISpriteFrame[] frames, out TypeDictionary metadata)
+		{
+			metadata = new TypeDictionary();
+
+			using (BinaryReader reader = new BinaryReader(s))
+			{
+				ushort unknown = reader.ReadUInt16();
+				FrameNum = reader.ReadUInt16();
+				unknown = reader.ReadUInt16();
+
+				byte[] palette = new byte[256 * 3];
+				for (int i = 0; i < palette.Length; i++)
+				{
+					palette[i] = reader.ReadByte();
+				}
+
+				frames = ParseFrames(reader);
+			}
+
+			return true;
+		}
+	}
+}
